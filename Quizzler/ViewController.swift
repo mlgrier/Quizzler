78d8d507a4d9626d8da4c83ceb76f262dd0de4e0
Marco Grier <mlgrier@gmail.com>
2017-08-02 19:29:25 -0500

    update

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -21,10 +23,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
   
     }
     
